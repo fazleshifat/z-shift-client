f39e0139b98b1d905563f8f0fc2eba97c31f3155
fazleshifat <mailbox189.shifat@gmail.com>
2025-07-05 01:42:07 +0600

    added user info and payment histiry info and function to save at DB

--- src/pages/Dashboard/Payment/PaymentForm.jsx
+++ src/pages/Dashboard/Payment/PaymentForm.jsx
@@ -1,9 +1,10 @@
 import { CardElement, useElements, useStripe } from '@stripe/react-stripe-js';
 import { useQuery } from '@tanstack/react-query';
-import React, { useState } from 'react';
+import React, { use, useState } from 'react';
 import { useParams } from 'react-router';
 import useAxiosSecure from '../../../hooks/useAxiosSecure';
 import Spinner from '../../../components/Spinner';
+import { AuthContext } from '../../../contexts/AuthContext';
 
 const PaymentForm = () => {
 
@@ -11,6 +12,7 @@ const PaymentForm = () => {
     const elements = useElements();
     const { parcelId } = useParams();
     const axiosSecure = useAxiosSecure();
+    const { user } = use(AuthContext);
 
     const [errorMessage, setErrorMessage] = useState('');
 
@@ -60,7 +62,7 @@ const PaymentForm = () => {
 
 
         // step-2: create payment intent
-        const res = await axiosSecure.post('/create-payment-intent', {
+        const res = await axiosSecure.post('/create-payment', {
             amountInCents,
             parcelId
         })
@@ -70,7 +72,10 @@ const PaymentForm = () => {
         const result = await stripe.confirmCardPayment(clientSecret, {
             payment_method: {
                 card: elements.getElement(CardElement),
-                billing_details: 'Felix Tavio',
+                billing_details: {
+                    name: user?.displayName || 'Guest',
+                    email: user?.email || 'unknown@example.com',
+                }
             },
         });
 
@@ -81,13 +86,24 @@ const PaymentForm = () => {
             if (result.paymentIntent.status === 'succeeded') {
                 console.log('Payment Succeed');
                 console.log(result);
+
+                // mark parcel and create Payment History
+                const paymentData = {
+                    parcelId,
+                    email: user?.email,
+                    amount: amountInCents,
+                    transactionId: result.paymentIntent.id,
+                    paymentMethod: result.paymentIntent.payment_method_types,
+                }
+
+                await axiosSecure.post('/payments/confirm', paymentData);
             }
         }
     }
 
     return (
         <div>
-            <form onClick={handleSubmit} className='space-y-4 bg-white p-6 rounded-xl shadow-md w-full max-w-md mx-auto'>
+            <form onSubmit={handleSubmit} className='space-y-4 bg-white p-6 rounded-xl shadow-md w-full max-w-md mx-auto'>
                 <CardElement className='p-2 border rounded'></CardElement>
                 <button type='submit'
                     className='btn btn-primary w-full'
