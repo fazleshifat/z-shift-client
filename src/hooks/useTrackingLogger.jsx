7fec81b897738984dfd6a04a451ecb2cd427ec1d
fazleshifat <mailbox189.shifat@gmail.com>
2025-07-13 03:31:45 +0600

    created custom hook for implement the parcel tracking

---
+++ src/hooks/useTrackingLogger.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import useAxiosSecure from './useAxiosSecure';
+
+const useTrackingLogger = () => {
+
+    const axiosSecure = useAxiosSecure();
+
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default useTrackingLogger;
